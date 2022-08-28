f514d185df6c41ce606819dbf5b3337e397aed2a
Sonjoy Samadder <sonjoy88@gmail.com>
2022-08-28 17:35:21 +0600

    Vue.js added

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h3>{{ message }}</h3>
+  </div>
+</template>
+
+<script setup>
+import {ref} from "vue";
+
+const message = ref("Welcome");
+</script>
+
+<style scoped>
+h3 {
+  color: brown;
+}
+</style>
