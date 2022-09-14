d02ec7e9b743a328a64b1b89164fe4375c841ad0
Sonjoy Samadder <sonjoy88@gmail.com>
2022-09-14 08:16:13 +0600

    Invoice parser method draft - total added

--- src/components/templates/InvocieParser.vue
+++ src/components/templates/InvocieParser.vue
@@ -3,14 +3,12 @@
 </template>
 
 <script setup>
-import { computed, onMounted, ref } from "vue";
+import {computed, onMounted, ref} from "vue";
 import state from "../../compositions/settings.js";
 import Basic from "../../templates/Basic.svg";
 
 function getAndModifySVG(url) {
-  return fetch(url)
-    // Get SVG response as text
-    .then(response => response.text())
+  return fetch(url).then(response => response.text())
 }
 
 const total = computed(function () {
@@ -20,19 +18,14 @@ const total = computed(function () {
 });
 
 const markup = ref("<span>loading</span>");
-const invoice = computed( function () {
+const invoice = computed(function () {
   return parser(markup.value);
 });
-
-const mapper = {
-  "%INVOICE_NUMBER%": "invoice",
-  "%INVOICE_DATE%": "number",
-  "%BILL_TO%": "billTo",
-};
 
 function parser(svg) {
   svg = svg.replace("%INVOICE_NUMBER%", state.settings.number);
   svg = svg.replace("%INVOICE_DATE%", state.settings.date);
+  svg = svg.replace("%TOTAL%", total.value);
 
   return svg;
 }
@@ -45,8 +38,8 @@ function formatNumber(number) {
   return number.toFixed(2);
 }
 
-onMounted(function() {
-  getAndModifySVG(Basic).then(function(text) {
+onMounted(function () {
+  getAndModifySVG(Basic).then(function (text) {
     markup.value = text;
   });
 })
